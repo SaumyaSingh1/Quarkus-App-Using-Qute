57c64838e0bde4a3a117fb53f936ded9ed1d2d3b
SaumyaSingh1 <ssaumyas100@gmail.com>
2021-01-09 17:57:35 +0530

    Trying Qute Template | Quarkus

---
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -0,0 +1,17 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+
+public class GreetingResourceTest {
+
+
+    public void testHelloEndpoint() {
+
+    }
+
+}
